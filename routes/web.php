3ba7bece33632ba7605200b00b1ed402e48db904
marwan-tabib <marwanmaroc297@gmail.com>
2022-04-17 14:19:50 +0200

    3. Query events and transactions.ar

--- routes/web.php
+++ routes/web.php
@@ -14,32 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    //--- ((( PDO ))) ---//
-        // connection data base with PDO , par default connection with mysql
-        $pdo = DB::connection()->getPdo();
-        $users = $pdo->query('select * from users')->fetchAll();
+    // DB::transaction(function () {
+    //     DB::table('users')->where('id' , 32)->delete();
+    //     $result = DB::table('users')->where('id',4)->update(['name' => 'none']);
+    //     dump('result' , $result);
+    // }, 3);
 
-        /* Connection database with pdo sqlite
-        $pdo = DB::connection('sqlite')->getPdo();*/
 
-    //--- ((( Query Builder ))) ---//
-        // Selcet Data with DB facade
-        $result = DB::select('select * from users');
-        $result_id = DB::select('select * from users where id=?' , [1]);
-        $result_id_multi_sector = DB::select('select * from users where id=:id' , ['id' => 1]); 
-        // Insert data with DB Facade 
-        $insert = DB::insert('insert into users (name , email , password) values (?,?,?)' , ['Mike' , 'ddzl@mail.fb' , 'password123']);
-        // Update data with DB Facade 
-        $affected = DB::update('update users set email = "nawemfail@mail.fb" where id=?' , [1]);
-        // Delete data with Db Facade 
-        $dalete = DB::delete('delete from users where id=?' , [4]);
-    
-        dump("result" , $result);
-        dump("result_id" , $result_id);
-        dump("result_id_multi_sector" , $result_id_multi_sector);
-        dump("insert" , $insert);
-        //dump("affected" , $affected);
-        dump("dalete" , $dalete);
-    
+    DB::transaction(function () {
+        DB::update('update users set name = 11');
+     
+        DB::delete('delete from users where id=32');
+    });
+
+
+
+    //DB::table('users')->insert(['name' => 'mike' , 'email' => 'mike@ff.ff' , 'password' => 'password123']);
     return view('welcome');
 });
