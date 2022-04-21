f90ac0da5b396391fd536945c61da9738ce4ece6
marwan-tabib <marwanmaroc297@gmail.com>
2022-04-21 05:04:48 +0200

    3. WHERE sql clause – part 2

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(CommentSeeder::class);
         $this->call(RoomSeeder::class);
+        $this->call(ReservationSeeder::class);
     }
 }
