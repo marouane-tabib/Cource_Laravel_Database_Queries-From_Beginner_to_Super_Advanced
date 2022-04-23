f3c7ab461a809b43624ee9321e180706917bc94c
marwan-tabib <marwanmaroc297@gmail.com>
2022-04-23 18:51:54 +0200

    4. WHERE sql clause – part 3

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,5 +24,15 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'meta' => [
+            'setting' =>[
+                'site_bg' => 'black',
+                'site_language' => 'en'
+            ],
+            'skills' => $faker->randomElements([
+                'laravel','PHP7','Wordpress','HTML5','CSS','React JS'
+            ], mt_rand(1,6)),
+            'gender' => $faker->randomElements(['Mole' , 'Famele'])
+        ]
     ];
 });
