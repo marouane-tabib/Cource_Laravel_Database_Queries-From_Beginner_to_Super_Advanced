437902e481e77844d77fc1ade94d1ecd491e25f2
marwan-tabib <marwanmaroc297@gmail.com>
2022-04-26 04:40:19 +0200

    7. Raw sql expressions

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,36 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    //$result = DB::statement('ALTER TABLE comments ADD FULLTEXT fulltext_index(content)');
-    //$result = DB::table('comments')->whereRaw('MATCH(content) AGAINST (? IN BOOLEAN MODE)' , ['Rerum'])->get();
-    //$result = DB::table('comments')->where('content' , 'like' , '%Rerum%')->get();
-    $result = DB::table('comments')->whereRaw('MATCH(content) AGAINST (? IN BOOLEAN MODE)' , ['+Rerum -corporis'])->get();
 
+    // $result = DB::table('comments')
+    // // ->where("content", 'like', '%inventore%')
+    // ->whereRaw("content LIKE '%inventore%'") // be careful about SQL injections!
+    // // ->where(DB::raw("content LIKE '%inventore%'")) // not working because where() needs two parameters
+    // ->get();
+
+    // $result = DB::table('comments')
+    //     // ->select(DB::raw('count(user_id) as number_of_comments, users.name'))
+    //     ->selectRaw('count(user_id) as number_of_comments, users.name',[])
+    //     ->join('users','users.id','=','comments.user_id')
+    //     ->groupBy('user_id')
+    //     ->get();
+
+    // whereRaw / orWhereRaw
+    // havingRaw / orHavingRaw
+    // orderByRaw
+    // groupByRaw
+
+    // $result = DB::table('comments')
+    //             ->orderByRaw('updated_at - created_at DESC')
+    //             ->get();
+
+    $result = DB::table('users')
+                ->selectRaw('LENGTH(name) as name_lenght, name')
+                ->orderByRaw('LENGTH(name) DESC')
+                ->get();
+                
     dump($result);
+
 
     
     return view('welcome');
