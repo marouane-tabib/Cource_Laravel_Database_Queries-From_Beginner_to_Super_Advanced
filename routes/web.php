22fa854423e6a90d3f730534c6852d3d4605e3e9
marwan-tabib <marwanmaroc297@gmail.com>
2022-06-10 18:26:38 +0200

    11. UNION select clause

--- routes/web.php
+++ routes/web.php
@@ -16,77 +16,30 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-    // $result = DB::table('reservations')
-    //         ->join('rooms','reservations.room_id', '=', 'rooms.id')
-    //         ->join('users','reservations.user_id', '=', 'users.id')
-    //         ->where('rooms.id', '>', 3)
-    //         ->where('users.id', '>', 1)
-    //         ->get();
-
-    // $result = DB::table('reservations')
-    //         ->join('rooms', function($join) {
-    //             $join->on('reservations.room_id', '=', 'rooms.id')
-    //             ->where('rooms.id', '>', 3);
-    //         })
-    //         ->join('users', function($join) {
-    //             $join->on('reservations.user_id', '=', 'users.id')
-    //             ->where('users.id', '>', 1);
-    //         })
-    //         ->get();
-
-
-    // $rooms = DB::table('rooms')
-    //         ->where('id', '>', 3);
-
     // $users = DB::table('users')
-    //         ->where('id', '>', 1);
+    //         ->select('name');
 
-    // $result = DB::table('reservations')
-    //     ->joinSub($rooms, 'rooms', function ($join) {
-    //         $join->on('reservations.room_id', '=', 'rooms.id');
-    //     })
-    //     ->joinSub($users, 'users', function ($join) {
-    //         $join->on('reservations.user_id', '=', 'users.id');
-    //     })
-    //     ->get();
-
-
-    // $result = DB::table('rooms')
-    //         ->leftJoin('reservations','rooms.id', '=', 'reservations.room_id')
-    //         ->selectRaw('room_size, count(reservations.id) as reservations_count')
-    //         ->groupBy('room_size')
-    //         ->orderByRaw('count(reservations.id) DESC')
+    // $result = DB::table('cities')
+    //         ->select('name')
+    //         ->union($users)
     //         ->get();
 
-    // $result = DB::table('rooms')
-    //         ->leftJoin('reservations','rooms.id', '=', 'reservations.room_id')
-    //         ->selectRaw('room_size, price, count(reservations.id) as reservations_count')
-    //         ->groupBy('room_size','price')
-    //         ->get();
+    $room = DB::table('comments')
+            ->select('rating as rating_or_room_id','id', DB::raw('"comments" as type_of_activity'))
+            ->where('user_id',2);
 
-    // $result = DB::table('rooms')
-    //         ->leftJoin('reservations', 'rooms.id', '=', 'reservations.room_id')
-    //         ->leftJoin('cities', 'reservations.city_id', '=', 'cities.id')
-    //         ->selectRaw('room_size, count(reservations.id) as reservations_count, cities.name')
-    //         ->groupBy('room_size','cities.name')
-    //         ->orderByRaw('count(reservations.id) DESC')
-    //         ->get();
-
-    $result = DB::table('rooms')
-            ->crossJoin('cities')
-            ->leftJoin('reservations', function($join){
-                $join->on('rooms.id', '=', 'reservations.room_id')
-                ->on('cities.id','=','reservations.city_id');
-            })
-            // ->selectRaw('room_size, count(reservations.id) as reservations_count, cities.name')
-            ->selectRaw('count(reservations.id) as reservations_count, cities.name')
-            ->groupBy(/*'room_size',*/'cities.name')
-            ->orderByRaw('count(reservations.id) DESC')
-            // ->orderBy('rooms.room_size', 'DESC')
+    $result = DB::table('reservations')
+            ->select('room_id as rating_or_room_id', 'id', DB::raw('"reservation" as type_of_activity'))
+            ->union($room)
+            ->where('user_id',2)
             ->get();
+
 
     dump($result);
 
     return view('welcome');
 });
 
+// order_id  | client_id | order_date | order_amount
+
+// refund_id  | client_id | refund_date |  refund_amount 
